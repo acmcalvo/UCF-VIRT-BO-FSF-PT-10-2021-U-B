a0666af3ddd79cdc6a125ed8fbae6f519a9f7fe0
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2021-10-26 23:19:49 -0400

    Update script.js

--- Class-Content/03-JavaScript/01-Activities/02-Iteration/Unsolved/script.js
+++ Class-Content/03-JavaScript/01-Activities/02-Iteration/Unsolved/script.js
@@ -1,8 +1,8 @@
 // TODO: Write Your JavaScript Code Here
-var students = ['Sarah','Orlando','Heater','Ismael','Hung'];
+const students = ['Jeffry', 'Johnson', 'Anthony', 'Ronny', 'Daniel'];
 
 console.log(students.length);
 
-for (var i = 0; i < students, length; i++) {
-    console.log('Great to se you, ' + students[i] + '!'); 
+for (let i = 0; i < students.length; i++) {
+  console.log(`Great to se you, {students[i]}!`);
 }
